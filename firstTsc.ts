25bd12ca777088c53b42f07d425085d2bc0f81a8
shuvro baset <md.abdul.baset75@gmail.com>
2021-09-10 10:04:43 +0600

    typeScript initialize

---
+++ firstTsc.ts
@@ -0,0 +1,4 @@
+// type script is very strict for type. We can't change the type of the variable later.
+let a: number = 35;
+let b: string = "hello"
+let c: boolean = false;
---
+++ js/firstTsc.js
@@ -0,0 +1,5 @@
+"use strict";
+// type script is very strict for type. We can't change the type of the variable later.
+let a = 35;
+let b = "hello";
+let c = false;
